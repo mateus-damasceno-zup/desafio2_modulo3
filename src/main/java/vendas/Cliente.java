a33b4b72a0d6e99325cda44792bf0249b38bce86
mateus-damasceno-zup <mateus.damasceno@zup.com.br>
2023-07-05 21:13:12 -0300

    heranca cliente e vendedor

--- src/main/java/vendas/Cliente.java
+++ src/main/java/vendas/Cliente.java
@@ -1,50 +1,10 @@
 package vendas;
 
-public class Cliente {
-
-    private String nome;
-    private String cpf;
-    private String email;
-
+public class Cliente extends Pessoa {
     public Cliente(String nome, String cpf, String email) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-    }
-    public Cliente(){
-
+        super(nome, cpf, email);
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "Cliente{" +
-                "nome='" + nome + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", email='" + email + '\'' +
-                '}'+"\n";
+    public Cliente() {
     }
 }
